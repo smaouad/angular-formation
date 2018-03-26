e350c9c4604a2404b4f108d7c8cb270f8aa20cc6
smaouad <aouadsaid@gmail.com>
2018-03-26 17:01:46 +0200

    Install bootstrap and ng-bootstrap

--- angular-formation/src/app/app.module.ts
+++ angular-formation/src/app/app.module.ts
@@ -5,6 +5,7 @@ import { CoreModule } from './core/core.module';
 import { Error404Module } from './error404/error404.module';
 import { HomeModule } from './home/home.module';
 import { SharedModule } from './shared/shared.module';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppComponent } from './app.component';
 
@@ -19,6 +20,7 @@ import { AppComponent } from './app.component';
     SharedModule,
     HomeModule,
     Error404Module,
+    NgbModule.forRoot()
   ],
   declarations: [
     AppComponent
--- angular-formation/src/app/error404/error404.module.ts
+++ angular-formation/src/app/error404/error404.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Error404Component } from './error404/error404.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: []
+  declarations: [
+    Error404Component
+  ],
+  exports: [
+    Error404Component
+  ]
 })
 export class Error404Module { }
