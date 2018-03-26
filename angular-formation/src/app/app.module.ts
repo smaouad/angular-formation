be449570d47c5dd3733ebea41474ee3da7630172
smaouad <aouadsaid@gmail.com>
2018-03-26 15:48:54 +0200

    generate components

--- angular-formation/src/app/app.module.ts
+++ angular-formation/src/app/app.module.ts
@@ -2,15 +2,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { CoreModule } from './core/core.module';
+import { Error404Module } from './error404/error404.module';
+import { HomeModule } from './home/home.module';
+import { SharedModule } from './shared/shared.module';
 
 import { AppComponent } from './app.component';
+
+
 
 
 
 @NgModule({
   imports: [
     BrowserModule,
-    CoreModule
+    CoreModule,
+    SharedModule,
+    HomeModule,
+    Error404Module,
   ],
   declarations: [
     AppComponent
