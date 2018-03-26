434fede4d823c938c3cb8b41ba333ab8505b4306
smaouad <aouadsaid@gmail.com>
2018-03-26 15:16:08 +0200

    Creation du module core et shared

--- angular-formation/src/app/app.module.ts
+++ angular-formation/src/app/app.module.ts
@@ -1,16 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { CoreModule } from './core/core.module';
 
 import { AppComponent } from './app.component';
 
 
+
 @NgModule({
+  imports: [
+    BrowserModule,
+    CoreModule
+  ],
   declarations: [
     AppComponent
-  ],
-  imports: [
-    BrowserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
